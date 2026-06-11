b7b9e5b554d4296e57c99b0a154ba3df944cd9b6
professorivanlopesmusica-boop <professorivanlopesmusica@gmail.com>
2026-06-11 15:15:25 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -366,7 +366,7 @@ export default function App() {
     `;
 
     try {
-      const data = await fetchWithRetry(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`, {
+     const data = await fetchWithRetry(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
